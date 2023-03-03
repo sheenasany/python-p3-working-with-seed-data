99fe405a24a39fc8ad6a8a4c3f56d53e178d3d4c
Sheena Sang <sheenasany@gmail.com>
2023-03-03 18:16:13 -0500

    completed

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,19 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    
+# Add a console message so we can see output when the seed file runs    
+print("Seeding games...")    
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+for i in range(50)]
+
+session.query(Game).delete()
+session.bulk_save_objects(games)
+session.commit()
